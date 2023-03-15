9e74478bc33d96572b103b8e4b2e6ac937db4ab8
ааа <https://github.com/@DariyaFernandes/Homework1/pull/1>
2023-03-16 00:14:03 +0300

    сделала 3е домаш задание

---
+++ src/Main.java
@@ -0,0 +1,101 @@
+public class Main {
+    public static void main(String[] args) {
+        byte bananas = 10;
+        System.out.println("Значение переменной byte с типом целочисленный равно " + bananas );
+        short apples = 250;
+        System.out.println("Значение переменной short с типом целочисленный равно " + apples );
+        int cherry = 50000;
+        System.out.println("Значение переменной int с типом целочисленный равно " + cherry );
+        long cat = 2150000000L;
+        System.out.println("Значение переменной long с типом целочисленный равно " + cat );
+        float v = 110.6f;
+        System.out.println("Значение переменной float с типом с плавающей точкой равно " + v );
+        double dog = 207390485.78;
+        System.out.println("Значение переменной double  с типом с плавающей точкой равно " + dog );
+        float c = 27.12f;
+        System.out.println(c);
+        long n = 987678965549L;
+        System.out.println(n);
+        float bv = 2.786f;
+        System.out.println(bv);
+        short cf = 569;
+        System.out.println(cf);
+        short df = -159;
+        System.out.println(df);
+        short as = 27897;
+        System.out.println(as);
+        byte qw = 67;
+        System.out.println(qw);
+        byte an = 23;
+        byte ass = 27;
+        byte ea = 30;
+        int dd = an + ass + ea;
+        int vss = 480 / dd;
+        System.out.println("На каждого ученика рассчитанно " + vss + " листов бумаги");
+        byte one = 16 / 2;
+        System.out.println("кол-во бутылок в минуту " + one);
+        int azq = one * 20;
+        System.out.println("За 20 минут машина произвела " + azq + " штук бутылок" );
+        int wsx = one * 1440;
+        System.out.println("За сутки машина произвела " + one + " штук бутылок");
+        int edc = one * 4320;
+        System.out.println("За 3 дня машина произвела " + one + " штук бутылок");
+        int rfv = one + 43800;
+        System.out.println("За 1 месяц машина произвела " + rfv + " штук бутылок");
+        byte y = 120;
+        byte tgb = 4 + 2;
+        int ujm = y / tgb;
+        int b = ujm * 2;
+        int k = ujm * 4;
+        System.out.println("В школе,где " + ujm + " классов, нужно " + b + " банок белой краски и " + k + " банок коричневой краски");
+        short banana = 5 * 80;
+        short milk = 2 * 105;
+        short ice = 2 * 100;
+        short ege = 4 * 70;
+        int blender = banana + milk + ice + ege;
+        System.out.println("Кол-во граммов спортзавтрака " + blender);
+        int kg = blender / 1000;
+        System.out.println("Кол-во килограмм в спортзавтраке " + kg);
+        float h = 7 / 0.25f;
+        System.out.println("Кол-во дней чтобы сбросить 7 кг " + h);
+        float j = 7 / 0.5f;
+        System.out.println("Кол-во дней чтобы сбросить 7 кг " + j);
+        short misha = 67760 / 100 * 10;
+        int receivesMisha = 67760 + misha;
+        int dssf = receivesMisha * 12;
+        int mishaYear = 67760 * 12;
+        int differenceMisha = dssf - mishaYear;
+        System.out.println("Миша теперь получает " + receivesMisha + " Годовой доход вырос на " + differenceMisha + " рублей");
+        short denis = 83690 / 100 * 10;
+        int receivesDenis = 83690 + denis;
+        int rifn = receivesDenis * 12;
+        int denisYear = 83690 * 12;
+        int differenceDenis = rifn - denisYear;
+        System.out.println("Денис теперь получает " + receivesDenis + " Годовой доход вырос на " + differenceDenis);
+        short kristina = 76230 / 100 * 10;
+        int receivesKristina = 76230 + kristina;
+        int ikm = receivesKristina * 12;
+        int krisYear = 76230 * 12;
+        int differenceKris = ikm -krisYear;
+        System.out.println("Кристина теперь получает " + receivesKristina + " Годовой доход вырос на " + differenceKris);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
