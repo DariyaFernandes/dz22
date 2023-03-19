47ca4673bc13f821ca6ec086ea706467d685b0f2
ааа <https://github.com/@DariyaFernandes/Homework1/pull/1>
2023-03-19 20:59:11 +0300

    сделала 4 дз

---
+++ homework4/home44/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    int age = 18;
+    System.out.println
+    if (age >= 18) {
+        System.out.println("Если возраст человека равен " + human + " то он совершеннолетний");
+    }
+
+}
---
+++ homework4/home444/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int human = 18;
+        if (human >= 18) {
+            System.out.println("Если возраст человека равен " + human + " то он совершеннолетний");
+            int children = 17;
+            if (children <18);
+            {
+                System.out.println("Если возраст человека равен " + children + " , то он не достиг совершеннолетия, нужно немного подождать");
+            }
+            int temperature = 2;
+            if (temperature < 5) {
+                System.out.println("На улице " + temperature + " градуса, нужно надеть шапку");
+            }
+                int hot = 10;
+                if (hot > 5) {
+                System.out.println("На улице " + hot + " градусов, можно идти без шапки");
+                int speed = 65;
+                if (speed > 60) {
+                    System.out.println("Если скорость " + speed + ", придется заплатить штраф");
+                }
+                int speedd = 50;
+                if (speedd < 60) {
+                    System.out.println("Если скорость " + speedd + ", можно ездить спокойно");
+                }
