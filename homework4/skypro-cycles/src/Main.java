629220b6850521787cb039cc456037215fbaa715
ааа <https://github.com/@DariyaFernandes/Homework1/pull/1>
2023-03-27 20:36:06 +0300

    циклы

---
+++ homework4/skypro-cycles/src/Main.java
@@ -0,0 +1,56 @@
+public class Main {
+    public static void main (String[] args) {
+        for (int i = 1; i <= 10; i = i + 1) {
+            System.out.println("Итерация цикла " + i);
+        }
+        for (int i = 10; i >= 1; i = i - 1) {
+            System.out.println("Итерация цикла " + i);
+        }
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println("Итерация цикла " + i);
+        }
+        for (int i = -10; i <= 10; i = i + 1) {
+            System.out.println("Итерация цикла " + 1);
+        }
+        for ( int i = 1904; i < 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
+
+        }
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(+ i);
+        }
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(+ i);
+        }
+        int salary = 29000;
+        int total = 0;
+        total = total + salary;
+        System.out.println("Месяц " + 0 + " сумма накоплений равна " + total);
+        total = total + salary;
+        System.out.println("Месяц " + 1 + " сумма накоплений равна " + total);
+        total = total + salary;
+        System.out.println("Месяц " + 2 + " сумма накоплений равна " + total);
+        total = total + salary;
+        System.out.println("Месяц " + 3 + " сумма накоплений равна " + total);
+        total = total + salary;
+        System.out.println("Месяц " + 4 + " сумма накоплений равна " + total);
+        total = total + salary;
+        System.out.println("Месяц " + 5 + " сумма накоплений равна " + total);
+        total = total + salary;
+        System.out.println("Месяц " + 6 + " сумма накоплений равна " + total);
+        total = total + salary;
+        System.out.println("Месяц " + 7 + " сумма накоплений равна " + total);
+        total = total + salary;
+        System.out.println("Месяц " + 8 + " сумма накоплений равна " + total);
+        total = total + salary;
+        System.out.println("Месяц " + 9 + " сумма накоплений равна " + total);
+        total = total + salary;
+        System.out.println("Месяц " + 10 + " сумма накоплений равна " + total);
+        total = total + salary;
+        System.out.println("Месяц " + 11 + " сумма накоплений равна " + total);
+
+    }
+        }
+
+
+}
